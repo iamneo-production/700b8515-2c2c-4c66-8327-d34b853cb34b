93e243eb15153c41433ba0c3b502a38306f6b936
nandhini213 <nandhinisampath01@gmail.com>
2023-06-29 04:00:52 +0000

    done

--- reactapp/src/App.test.js
+++ reactapp/src/App.test.js
@@ -1,10 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3ad305b833185ec1951b924e070a6f978eb5f057
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
