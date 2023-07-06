0f41b6c0de972260887c11ec9bf500b6eac241ea
Sruthi08-sudo <sruthivarma38@gmail.com>
2023-07-06 14:27:00 +0000

    Project

---
+++ reactapp/src/Card.jsx
@@ -0,0 +1,47 @@
+
+import PropTypes from 'prop-types'
+const policyData = [
+  {
+    policyName: 'Life insurance',
+    yourPolicyId: '928888211',
+    applicableAge: '5 to 50',
+    policyDetails: 'aaa....',
+    numberOfYearsPlan: '10 years',
+    claimAmount: '2,00,000',
+    termsAndConditions: 'xx.....',
+  },
+]
+
+export default function Card(props) {
+  return (
+    <div style={{paddingLeft:props.paddingl,alignContent:'center'}}>
+        <div className="card my-5 mx-5" style={{width:'50rem'}}>
+            <div className="card-body d-flex" id1={props.id1}>
+              <div className='container-lg p-2 flex-fill'>
+                <h5 className="my-3">Policy name: {policyData[0].policyName}</h5>
+                
+                <h5 className="my-3">Applicable age: {policyData[0].applicableAge}</h5>
+                
+                <h5 className="my-3">Number of years plan:{props.numberOfYearsPlan} </h5>
+                <h5 className="my-3">Claim Amount: {policyData[0].claimAmount}</h5>
+              </div>
+              <div className='container-lg p-2 flex-fill'>
+                <h5 className="my-3">Your policy id: {policyData[0].yourPolicyId}</h5>
+                <h5 className="my-3">Policy Details: {policyData[0].policyDetails}</h5>
+                <h5 className="my-3">Terms and Conditions: {policyData[0].termsAndConditions}</h5>
+              </div>
+            </div>
+            
+            <div  className='mx-5' style={{paddingLeft:"550px",display:props.c1}}>
+              <a href="/" className="btn btn-primary" id="deletePolicy">Delete</a>
+              <a href="/" className="btn btn-primary mx-2" id="editPolicy">Edit</a>
+            </div>
+            <div  className='mx-5' style={{paddingLeft:"550px",display:props.c2,paddingBottom:'10px'}}>
+              <a href="/" className="btn btn-primary mx-2">Select plan</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+Card.propTypes={c1:PropTypes.string};
+Card.defaultProps={c1:'none',c2:'none',id1:'set id1',numberOfYearsPlan:'set numberOfYearsPlan',paddingl:'450px'}
