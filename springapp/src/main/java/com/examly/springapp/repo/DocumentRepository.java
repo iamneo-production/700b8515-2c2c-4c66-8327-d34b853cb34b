8936f0d0dc80ba5c79c8f4202d95699e4b50135a
SOFIAH2411 <sofiahfyrose@gmail.com>
2023-07-13 15:38:44 +0000

    git update1

---
+++ springapp/src/main/java/com/examly/springapp/repo/DocumentRepository.java
@@ -0,0 +1,7 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+    // Additional custom query methods, if needed
+}
