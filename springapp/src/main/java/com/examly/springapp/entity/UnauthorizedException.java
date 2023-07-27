f072e40949d646ca97e02fc0630b8d2f6f5878f5
suyashgholap <suyashsgholap@gmail.com>
2023-07-27 16:35:55 +0000

    reverse springapp

--- springapp/src/main/java/com/examly/springapp/entity/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.examly.springapp.entity;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
