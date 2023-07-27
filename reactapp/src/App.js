befcf2128c8125c82e2d5b68a71921ba93c4ad6b
LeelaRatnaSree <130282681+LeelaRatnaSree@users.noreply.github.com>
2023-07-28 00:55:29 +0530

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Login from './Login/Login'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from './Signup/Signup';
+import Userviewpolicy from './Userviewpolicy/Userviewpolicy'
+import Adminaddpolicy from './Adminaddpolicy/Adminaddpolicy'
+import Userprofile from './Userprofile/Userprofile'
+import Userappliedpolicy from './Userappliedpolicy/Userappliedpolicy'
+import Adminappliedpolicy from './Adminappliedpolicy/Adminappliedpolicy'
+import Admineditappliedpolicy from './Admineditappliedpolicy/Admineditappliedpolicy'
+import Adminviewapplicants from './Adminviewapplicants/Adminviewapplicants'
+import Admineditapplicants from './Admineditapplicants/Admineditapplicants'
+import Adminsignup from './Adminsignup/Adminsignup'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/Userviewpolicy" element={<Userviewpolicy />} />
+          <Route path="/Userprofile" element={<Userprofile />} />
+          <Route path="/Userappliedpolicy" element={<Userappliedpolicy/>} />
+          <Route path="/Adminaddpolicy" element={<Adminaddpolicy/>} />
+          <Route path="/Adminappliedpolicy" element={<Adminappliedpolicy/>} />
+          <Route path="/Admineditappliedpolicy" element={<Admineditappliedpolicy/>} />
+          <Route path="/Adminviewapplicants" element={<Adminviewapplicants/>} />
+          <Route path="/Admineditapplicants" element={<Admineditapplicants/>} />
+          <Route path="/Adminsignup" element={<Adminsignup/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
