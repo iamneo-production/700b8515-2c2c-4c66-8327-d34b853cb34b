c690729f103b6e589f7623ca6256514647aef870
LeelaRatnaSree <leelasreeratna@gmail.com>
2023-07-31 14:19:40 +0000

    springsrcadded

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,13 +1,18 @@
-package com.example.springapp;
+package com.examly.springapp;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories("com.examly.springapp.repo")
+@EntityScan("com.examly.springapp.entity")
 public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
-
 	}
 
 }
