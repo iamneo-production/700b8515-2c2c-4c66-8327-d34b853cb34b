6da228ed15a7f47c237f05cfcdeae14d0ec15380
Sindhujars <113293040+Sindhujars@users.noreply.github.com>
2023-07-31 23:56:49 +0530

    Update DocumentRepository.java

--- springapp/src/main/java/com/examly/springapp/repo/DocumentRepository.java
+++ springapp/src/main/java/com/examly/springapp/repo/DocumentRepository.java
@@ -1,7 +1,11 @@
+package com.examly.springapp.repo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.examly.springapp.model.DocumentModel;
+
 @Repository
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-    // Additional custom query methods, if needed
+public interface DocumentRepository extends JpaRepository<DocumentModel, Long> {
+    // Add any additional repository methods if needed
 }
