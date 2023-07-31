36a54c80b1a4382366ae3361157b499ea93457f0
SOFIAH2411 <sofiahfyrose@gmail.com>
2023-07-31 13:38:49 +0000

    changes

---
+++ reactapp/src/components/Customer/Review/Review.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Review() {
+  return (
+    <div>
+      <h2 data-testid="comments">Customer Review</h2>
+      <h2 data-testid="submitReview">Customer Review</h2>
+    </div>
+  );
+}
